7d9864668f38ffc5926e88d17ce33292da84591b
Incrested <30969346+Incrested@users.noreply.github.com>
2017-11-05 07:07:08 -0800

    Added Script and Readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cookies
+Cookies is a JavaScript-based cookie analyzer and creator. It's purpose is to increase the efficiency of making cookies and reading them.
+
+### Install
+To install, simply add this code to your <head> tag:
+```
+  <script src="https://raw.githubusercontent.com/Incrested/Cookies/master/cookies.js"></script>
+```
---
+++ cookies.js
@@ -0,0 +1,21 @@
+function fetchCookies(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(i);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+}
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expire = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
